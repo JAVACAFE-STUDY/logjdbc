70c6b3404b197b8211fcf4391d18fa01608a579c
SejongPark <devsejong@gmail.com>
2016-07-23 17:01:50 +0900

    데이터베이스 타입 이넘 기능 개선

--- src/main/java/net/chandol/datasource/DatabaseType.java
+++ src/main/java/net/chandol/datasource/DatabaseType.java
@@ -11,29 +11,30 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 import static net.chandol.datasource.DatabaseTypeUtil.getDatabaseName;
-import static net.chandol.datasource.DatabaseTypeUtil.isMatchDatabaseName;
+import static net.chandol.datasource.DatabaseTypeUtil.isMatchDBName;
 
 /**
  * 지원하는 데이터베이스 타입
  */
 public enum DatabaseType {
-    MYSQL(MysqlParameterConverter.class),
-    MARIADB(MysqlParameterConverter.class),
-    H2(BaseParameterConverter.class),
-    ORACLE(NeedImplementParameterConverter.class),
-    POSTGRESQL(NeedImplementParameterConverter.class),
-    UNKNOWN(BaseParameterConverter.class);
+    MYSQL(MysqlParameterConverter.class, "MySql"),
+    H2(BaseParameterConverter.class, "H2"),
+    ORACLE(NeedImplementParameterConverter.class, "Oracle"),
+    POSTGRESQL(NeedImplementParameterConverter.class, "PostgreSQL"),
+    UNKNOWN(BaseParameterConverter.class, null);
 
     /* enum필드 */
 
     // 컨버터 클래스가 들어와요!
     private Class<? extends ParameterConverter> converterClass;
     private ParameterConverter converter;
+    private String matchName;
 
     /* 생성자 */
     // Lazy로딩을 적용. 실제 인스턴스는 getParameterConverter가 호출될 때 생성한다.
-    DatabaseType(Class<? extends ParameterConverter> converterClass) {
+    DatabaseType(Class<? extends ParameterConverter> converterClass, String matchName) {
         this.converterClass = converterClass;
+        this.matchName = matchName;
     }
 
     // 컨버터를 반환한다. converter는 하나의 인스턴스만을 관리한다.
@@ -56,16 +57,18 @@ public enum DatabaseType {
     }
 
     // FIXME 데이터베이스명에 따라서 벤더를 선택할 수 있도록 만들 것.
-    // FIXME 생성자로 매칭문자열을 받을 것.
+    // TODO 리펙토링 필요.
     public static DatabaseType find(DataSource datasource) {
         String name = getDatabaseName(datasource);
 
-        if (isMatchDatabaseName(name, "H2"))
-            return H2;
-        else if (isMatchDatabaseName(name, "MySql"))
-            return MYSQL;
-        else
-            return UNKNOWN;
+        // 여기서 복잡도가 너무 높음.
+        // 자바7이 기준. stream을 사용하지는 못함. ㅠㅠ
+        for (DatabaseType type : DatabaseType.values()) {
+            if (isMatchDBName(name, type.matchName)) {
+                return type;
+            }
+        }
+        return UNKNOWN;
     }
 }
 
@@ -83,7 +86,7 @@ final class DatabaseTypeUtil {
     }
 
     // DB명이 맞는지를 검사해본다.
-    static boolean isMatchDatabaseName(String databaseName, String typeName) {
+    static boolean isMatchDBName(String databaseName, String typeName) {
         return databaseName.toLowerCase()
                 .startsWith(typeName.toLowerCase());
     }
