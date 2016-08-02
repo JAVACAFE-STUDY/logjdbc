7fb57ffe31b2142b2046293ec4c2691a77dd23fa
hhj <gyahoo617@gmail.com>
2016-08-02 11:50:07 +0900

    jdbc 리소스 사용시 try-catch문 처리

--- src/test/java/net/chandol/logjdbc/LogJdbcDataSourceTest.java
+++ src/test/java/net/chandol/logjdbc/LogJdbcDataSourceTest.java
@@ -17,42 +17,48 @@ public class LogJdbcDataSourceTest extends AutoLogInitializer {
         DataSource h2DataSource = getDummyH2DataSource();
         DataSource dataSource = new LogJdbcDataSource(h2DataSource);
 
-        Connection connection = dataSource.getConnection();
+        //get the Connection
+        try(Connection connection = dataSource.getConnection();) {
 
-        Statement statement = connection.createStatement();
-        statement.execute("CREATE TABLE CUSTOMERS (ID INT NOT NULL, NAME VARCHAR (20) NOT NULL, AGE INT NOT NULL, ADDRESS CHAR (25), SALARY DECIMAL(18, 2), PRIMARY KEY (ID));");
-        statement.execute("INSERT INTO CUSTOMERS (ID, NAME, AGE, ADDRESS, SALARY) VALUES (2, 'Khilan', 25, 'Delhi', 1500.00 );");
-        statement.execute("INSERT INTO CUSTOMERS (ID, NAME, AGE, ADDRESS, SALARY) VALUES (3, 'kaushik', 23, 'Kota', 2000.00 );");
-        statement.execute("INSERT INTO CUSTOMERS (ID, NAME, AGE, ADDRESS, SALARY) VALUES (4, 'Chaitali', 25, 'Mumbai', 6500.00 );");
-        statement.execute("INSERT INTO CUSTOMERS (ID, NAME, AGE, ADDRESS, SALARY) VALUES (5, 'Hardik', 27, 'Bhopal', 8500.00 );");
-        statement.execute("INSERT INTO CUSTOMERS (ID, NAME, AGE, ADDRESS, SALARY) VALUES (6, 'Komal', 22, 'MP', 4500.00 );");
+          //Create table and Insert data
+          try(Statement statement = connection.createStatement();){
+              statement.execute("CREATE TABLE CUSTOMERS (ID INT NOT NULL, NAME VARCHAR (20) NOT NULL, AGE INT NOT NULL, ADDRESS CHAR (25), SALARY DECIMAL(18, 2), PRIMARY KEY (ID));");
+              statement.execute("INSERT INTO CUSTOMERS (ID, NAME, AGE, ADDRESS, SALARY) VALUES (2, 'Khilan', 25, 'Delhi', 1500.00 );");
+              statement.execute("INSERT INTO CUSTOMERS (ID, NAME, AGE, ADDRESS, SALARY) VALUES (3, 'kaushik', 23, 'Kota', 2000.00 );");
+              statement.execute("INSERT INTO CUSTOMERS (ID, NAME, AGE, ADDRESS, SALARY) VALUES (4, 'Chaitali', 25, 'Mumbai', 6500.00 );");
+              statement.execute("INSERT INTO CUSTOMERS (ID, NAME, AGE, ADDRESS, SALARY) VALUES (5, 'Hardik', 27, 'Bhopal', 8500.00 );");
+              statement.execute("INSERT INTO CUSTOMERS (ID, NAME, AGE, ADDRESS, SALARY) VALUES (6, 'Komal', 22, 'MP', 4500.00 );");
+          }catch (SQLException e){
+         	 connection.rollback();
+           }
 
-        PreparedStatement pstmt = connection.prepareStatement("SELECT * FROM CUSTOMERS WHERE ID=?");
-        pstmt.setLong(1, 3);
-        ResultSet resultSet = pstmt.executeQuery();
+          //Select the data
+          try(PreparedStatement pstmt = connection.prepareStatement("SELECT * FROM CUSTOMERS WHERE ID=?");
+              PreparedStatement pstmt2 = connection.prepareStatement("SELECT * FROM CUSTOMERS");
+              PreparedStatement pstmt3 = connection.prepareStatement("SELECT * FROM CUSTOMERS WHERE  NAME=?");){
 
-        while (resultSet.next()) {
-        }
+               pstmt.setLong(1, 3);
+               try(ResultSet resultSet = pstmt.executeQuery();){
+            	   while (resultSet.next()) {
+                   }
+               }
 
-        PreparedStatement pstmt2 = connection.prepareStatement("SELECT * FROM CUSTOMERS");
-        ResultSet resultSet2 = pstmt2.executeQuery();
+               try(ResultSet resultSet2 = pstmt2.executeQuery();){
+                   while (resultSet2.next()) {
+                   }
+               }
 
-        while (resultSet2.next()) {
-        }
-
-        PreparedStatement pstmt3 = connection.prepareStatement("SELECT * FROM CUSTOMERS WHERE  NAME=?");
-        pstmt3.setString(1, "Hardik");
-        ResultSet resultSet3 = pstmt3.executeQuery();
-
-        while (resultSet3.next()) {
-        }
-
-        resultSet.close();
-        resultSet2.close();
-        resultSet3.close();
-
-        connection.rollback();
-        connection.close();
+               pstmt3.setString(1, "Hardik");
+               try(ResultSet resultSet3 = pstmt3.executeQuery();){
+            	   while (resultSet3.next()) {
+                   }
+               }
+          }catch (SQLException e){
+        	 connection.rollback();
+          }
+          
+       }catch (SQLException e){
+      	 e.printStackTrace();
+       }
     }
-
 }
